4d498650f4aa41d9bcf985c884c5f0af001ed452
Monika Lenartowicz <nika.nel@interia.pl>
2023-03-22 15:25:04 +0100

    added DOM updating algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,6 +22,8 @@ const controlRecipes = async function () {
 
 		if (!id) return;
 		recipeView.renderSpinner();
+		// 0. update results view to mark selected search result
+		resultsView.update(model.getSearchResultsPage());
 
 		//  1. loading recipes
 		await model.loadRecipe(id);
@@ -70,7 +72,8 @@ const controlServings = function (newServings) {
 	model.updatedServings(newServings);
 
 	//update the recipe view
-	recipeView.render(model.state.recipe);
+	// recipeView.render(model.state.recipe);
+	recipeView.update(model.state.recipe);
 };
 
 const init = () => {
