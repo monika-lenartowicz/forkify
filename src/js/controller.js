3fe9974387163df33fd9f4d0f6ea43c68111a250
Monika Lenartowicz <nika.nel@interia.pl>
2023-03-23 15:38:15 +0100

    added control add bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -76,9 +76,17 @@ const controlServings = function (newServings) {
 	recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = function () {
+	if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+	else model.deleteBookmark(model.state.recipe.id);
+	console.log(model.state.recipe);
+	recipeView.update(model.state.recipe);
+};
+
 const init = () => {
 	recipeView.addHandlerRender(controlRecipes);
 	recipeView.addHandlerUpdateServings(controlServings);
+	recipeView.addHandlerAddBookmark(controlAddBookmark);
 	searchView.addHandlerSearch(controlSearchResult);
 	paginationView.addHandlerClick(controlPagination);
 };
