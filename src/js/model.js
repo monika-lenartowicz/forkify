6d87c65750af77bb2604a2068e586b65f08696f4
Monika Lenartowicz <nika.nel@interia.pl>
2023-03-24 11:57:19 +0100

    removed console log from model.js

--- src/js/model.js
+++ src/js/model.js
@@ -31,7 +31,6 @@ export const loadRecipe = async function (id) {
 
 		if (state.bookmarks.some(bookmark => bookmark.id === id)) state.recipe.bookmarked = true;
 		else state.recipe.bookmark = false;
-		console.log(state.recipe);
 	} catch (err) {
 		//temp error handling
 		console.error(`${err} 💥`);
