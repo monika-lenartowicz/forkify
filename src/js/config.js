f36fe4327f7bd606fad095cbc1dd469aae8b960a
Monika Lenartowicz <nika.nel@interia.pl>
2023-03-27 12:33:42 +0200

    added key and modal close variable to config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
+export const KEY = "f8977e63-6ba7-4c07-8b64-4c44cc93f24b";
+export const MODAL_CLOSE_SECONDS = 2500;
